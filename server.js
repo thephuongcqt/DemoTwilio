f2233e7a5593b36ce7b6230745d1dedf275d5a68
Nguyen The Phuong <thephuongcqt@gmail.com>
2018-07-05 20:00:36 +0700

    something

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var app = express();
-
-app.use("/", function(req, res){
-    res.end("Default Route")
-});
-
-var server = app.listen(process.env.PORT || 8080, function(){
-});
