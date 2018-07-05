85e1222e813a319f3bf3e3e5c36cdd201637af97
Nguyen The Phuong <thephuongcqt@gmail.com>
2018-07-05 21:03:18 +0700

    dynamic port

--- server.js
+++ server.js
@@ -30,4 +30,5 @@ app.use("/", function(req, res){
 });
 
 var server = app.listen(process.env.PORT || 8080, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
