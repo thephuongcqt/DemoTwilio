7c703c20273f8d54f133d4e82357e8300e3d31fa
Nguyen The Phuong <thephuongcqt@gmail.com>
2018-07-05 22:16:06 +0700

    change recording method

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.post("/Voice", function (req, res) {
     twiml.play("https://firebasestorage.googleapis.com/v0/b/callcenter2-79faf.appspot.com/o/audio%2Fdefault_greeting.mp3?alt=media&token=d91ed4a4-8a8e-4747-a047-d15397a9e15f");
     twiml.record({
         recordingStatusCallback: recordURL,
-        method: 'GET',
+        method: 'POST',
     });
     res.end(twiml.toString());
 });
